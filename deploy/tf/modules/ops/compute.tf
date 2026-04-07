25fdcbc20df239f9bde99d9c37db754e9987dcfc
Victor Martin <vmleon@gmail.com>
2026-04-07 19:57:33 +0200

    fix(deploy): add nginx config for Angular SPA and prefix ops hostname

--- deploy/tf/modules/ops/compute.tf
+++ deploy/tf/modules/ops/compute.tf
@@ -46,7 +46,7 @@ data "oci_core_images" "ol9_images" {
 resource "oci_core_instance" "instance" {
   availability_domain = lookup(var.ads[0], "name")
   compartment_id      = var.compartment_ocid
-  display_name        = "${var.project_name}${var.deploy_id}"
+  display_name        = "ops${var.project_name}${var.deploy_id}"
   shape               = var.instance_shape
 
   metadata = {
@@ -69,9 +69,9 @@ resource "oci_core_instance" "instance" {
   create_vnic_details {
     subnet_id                 = var.subnet_id
     assign_public_ip          = true
-    display_name              = "${var.project_name}${var.deploy_id}"
+    display_name              = "ops${var.project_name}${var.deploy_id}"
     assign_private_dns_record = true
-    hostname_label            = "${var.project_name}${var.deploy_id}"
+    hostname_label            = "ops${var.project_name}${var.deploy_id}"
   }
 
   source_details {
