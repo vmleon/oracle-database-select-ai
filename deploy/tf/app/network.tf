347503022c4aa665a561be19b12ca54c086ce667
Victor Martin <vmleon@gmail.com>
2026-04-16 18:35:28 +0200

    fix(infra): add service gateway route and force IPv4 in bootstrap

--- deploy/tf/app/network.tf
+++ deploy/tf/app/network.tf
@@ -47,6 +47,12 @@ resource "oci_core_route_table" "route_private" {
     destination_type  = "CIDR_BLOCK"
     network_entity_id = oci_core_nat_gateway.nat_gateway.id
   }
+
+  route_rules {
+    destination       = data.oci_core_services.all_services.services[0].cidr_block
+    destination_type  = "SERVICE_CIDR_BLOCK"
+    network_entity_id = oci_core_service_gateway.service_gateway.id
+  }
 }
 
 resource "oci_core_subnet" "public_subnet" {
